68cc951f077e3cfd2cf70bdd3045df5a7df1401a
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2020-11-01 22:43:07 +0900

    Revert "Revert "Revert "各モデルのテスト"""
    This reverts commit d0c3d471289539502a6340ca2ce36adc27bfd67c.

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-  end
-end
