13c4a0e35fd286213c959aa0ab9e6b3e03fb2898
yusei0726 <73167282+yusei0726@users.noreply.github.com>
2020-11-01 22:42:25 +0900

    Revert "各モデルのテスト"
    This reverts commit fb5c01148910b9df832d579e717d5a922a30e66a.

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,4 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users, dependent: :destroy
   has_many :messages, dependent: :destroy
-
-    validates :name, presence: true
-
 end
